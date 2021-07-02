d65fa6afb4cb20b9a2ee229b48538224f2ce6adb
Anderson Bastos Bandeira <emaildeandersonbastos@gmail.com>
2021-07-01 21:00:25 -0300

    Users CRUD done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,30 @@
 import {Request,Response} from "express";
 import { UserServices } from "../services/UserServices";
 class UserController{
-    async create(request:Request, response:Response){
+    async create(request: Request, response: Response){
         const {name, email, admin, password} = request.body;
         const userServices = new UserServices();
-        const user=await userServices.create({name, email, admin, password});
-        return response.json(user);
+        const user = await userServices.create({name, email, admin, password});
+        return response.status(201).json(user);
+    }
+    async delete(request: Request, response: Response){
+        const {email} = request.body;
+        const usersServices = new UserServices();
+        const result = await usersServices.delete({email});
+        return response.json(result);
+    }
+    async read(request: Request, response: Response){
+        const {name, email, admin} = request.body;
+        const usersServices = new UserServices();
+        const result = await usersServices.read({name, email, admin});
+        return response.json(result);
+    }
+    async update(request: Request, response: Response){
+        const {targetEmail} = request.body;
+        const {name, email, admin, password} = request.body;
+        const usersServices = new UserServices();
+        const result = await usersServices.update({email:targetEmail}, {name, email, admin, password});
+        return response.json(result);
     }
 }
 export{ UserController };
--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,4 +1,4 @@
-import { getRepository } from "typeorm";
+import { DeleteResult, getRepository } from "typeorm";
 import { hash } from "bcryptjs";
 import { User } from "../entities/User";
 import { v4 } from "uuid";
@@ -7,7 +7,7 @@ import { BaseError } from "./ErrorServices";
 
 interface IUserRequest{
     name?:string;
-    email:string;
+    email?:string;
     admin?:boolean;
     password?:string;
 }
@@ -37,7 +37,7 @@ class UserServices{
         await usersRepository.save(user);
         return classToPlain(user);
     }
-    async delete({email}:IUserRequest): Promise<void>{
+    async delete({email}:IUserRequest): Promise<DeleteResult>{
         if(!email){
             throw new BaseError(400, "Invalid e-mail");
         }
@@ -46,9 +46,53 @@ class UserServices{
             email
         });
         if(user){
-            const result = await usersRepository.delete(user);
+            return await usersRepository.delete({email});
         } else {
-            throw new BaseError(500, "Unable to delete user");
+            throw new BaseError(400, "User not found");
+        }
+    }
+    async read({name, email, admin}: IUserRequest): Promise<Record<string, User>>{
+        const usersRepository = getRepository(User);
+        let readTarget = {};
+        if(name){
+            readTarget["name"] = name;
+        }
+        if(email){
+            readTarget["email"] = email;
+        }
+        if(admin){
+            readTarget["admin"] = admin;
+        }
+        if(readTarget){
+            return classToPlain(usersRepository.find(readTarget));
+        } else {
+            throw new BaseError(400, "Nothing to find");
+        }
+    }
+    async update(target: IUserRequest, {name, email, admin, password}: IUserRequest): Promise<Record<string, User>>{
+        if(!target.email){
+            throw new BaseError(400, "Invalid user");
+        }
+        const usersRepository = getRepository(User);
+        const targetFound = await usersRepository.findOne({
+            email: target.email
+        });
+        if(targetFound){
+            if(name){
+                targetFound.name = name;
+            }
+            if(email){
+                targetFound.email = email;
+            }
+            if(admin){
+                targetFound.admin = admin;
+            }
+            if(password){
+                targetFound.password = await hash(password, 8);
+            }
+            return classToPlain(await usersRepository.save(targetFound));
+        } else {
+            throw new BaseError(400, "Invalid user");
         }
     }
 }
